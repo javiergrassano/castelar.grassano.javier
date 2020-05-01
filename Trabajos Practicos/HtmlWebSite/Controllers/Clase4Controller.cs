25dce57186c04959368961a58bbd662b570ad6ec
javier Grassano <javier.grassano@vindow.com>
2020-05-01 20:23:20 -0300

    Clase 4 - Maquetacion

---
+++ Trabajos Practicos/HtmlWebSite/Controllers/Clase4Controller.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace UAI2020TPS.Controllers
+{
+    public class Clase4Controller : Controller
+    {
+        // GET: Clase1
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult TP1()
+        {
+            return View();
+        }
+
+        public ActionResult TP2()
+        {
+            return View();
+        }
+
+        public ActionResult TP3()
+        {
+            return View();
+        }
+
+        public ActionResult TP4()
+        {
+            return View();
+        }
+
+        public ActionResult TP5()
+        {
+            return View();
+        }
+
+        public ActionResult TP6()
+        {
+            return View();
+        }
+
+        public ActionResult TP7()
+        {
+            return View();
+        }
+
+        public ActionResult TP8()
+        {
+            return View();
+        }
+
+    }
+}
